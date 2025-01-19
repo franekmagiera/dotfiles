1ee58806716f4707b1d1e8566acba2356f972762
franekmagiera <framagie@gmail.com>
2025-01-19 22:14:44 +0100

    Remove zsh setup and update python

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 brew bundle install --file=${BASEDIR}/Brewfile
 
-pyenv install 3.10.5
-pyenv global 3.10.5
-
-pip install codespell
+pyenv install 3.13
+pyenv global 3.13
 
 # Linking dotfiles.
 ln -sfv ${BASEDIR}/inputrc ~/.inputrc
@@ -15,10 +13,6 @@ ln -sfv ${BASEDIR}/inputrc ~/.inputrc
 # vim
 ln -sfv ${BASEDIR}/vimrc ~/.vimrc
 ln -sfv ${BASEDIR}/vimrc ~/.ideavimrc
-
-# zsh
-ln -sfv ${BASEDIR}/zshrc ~/.zshrc
-# ln -sfv ${BASEDIR}/zprofile ~/.zprofile
 
 # git
 ln -sfv ${BASEDIR}/gitconfig ~/.gitconfig
