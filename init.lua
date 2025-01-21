da07ae4ad2794fc606d5f23c9b84a2cee36461df
franekmagiera <framagie@gmail.com>
2025-01-21 22:41:52 +0100

    Add autocommands

--- init.lua
+++ init.lua
@@ -58,7 +58,9 @@ vim.opt.diffopt:append('indent-heuristic')
 -- Show a column at 80 characters as a guide for long lines.
 vim.opt.colorcolumn = '80'
 
--- Keymaps.
+--------------
+-- Keymaps. --
+--------------
 -- Easier way to move between windows.
 vim.keymap.set('n', '<C-j>', '<C-W>j') 
 vim.keymap.set('n', '<C-k>', '<C-W>k') 
@@ -81,6 +83,38 @@ vim.keymap.set('n', '<leader>sp', '[s')
 vim.keymap.set('n', '<leader>sa', 'zg')
 -- Show suggestions.
 vim.keymap.set('n', '<leader>s?', 'z=')
+
+-- Fast writing.
+vim.keymap.set('n', '<leader>w', ':w<cr>')
+
+-------------------
+-- Autocommands. --
+-------------------
+-- Highlight yanked text.
+vim.api.nvim_create_autocmd(
+	'TextYankPost',
+	{
+		pattern = '*',
+		command = 'silent! lua vim.highlight.on_yank({ timeout = 500 })'
+	}
+)
+
+-- Jump to last edit position on opening file.
+vim.api.nvim_create_autocmd(
+	'BufReadPost',
+	{
+		pattern = '*',
+		callback = function(ev)
+			if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+				-- Except for in git commit messages.
+				-- https://stackoverflow.com/questions/31449496/vim-ignore-specifc-file-in-autocommand
+				if not vim.fn.expand('%:p'):find('.git', 1, true) then
+					vim.cmd('exe "normal! g\'\\""')
+				end
+			end
+		end
+	}
+)
 
 -- Python.
 vim.g.python3_host_prog = '/Users/franek/.pyenv/shims/python3'
